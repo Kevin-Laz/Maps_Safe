530516f42d8771f2ff8eeb8e651eca99fa8f2498
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-10-27 22:57:07 -0500

    configuración onPush, standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ROUTES as HOME_ROUTES } from './feature/home/routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...HOME_ROUTES
+];
