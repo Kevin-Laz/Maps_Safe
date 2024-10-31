5a62e4255ee3a59bbed5a0b0b78555e86c90593b
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-10-31 18:45:51 -0500

    Se adiciono la visibilidad del modal login al sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatButtonModule, SideBarComponent, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, SideBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
