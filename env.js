bdc1da427ec81062ff647a454c4c34ddd3678d48
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-11-02 18:38:44 -0500

    generacion del enviroment en producción

---
+++ env.js
@@ -0,0 +1,13 @@
+const { writeFileSync } = require('fs');
+const { MAPS_API_KEY } = process.env;
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `
+  export const environment = {
+    production: true,
+    apiKey: '${MAPS_API_KEY}'
+  };
+`;
+
+writeFileSync(targetPath, envConfigFile);
