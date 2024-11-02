076f2ad8186b893045fe788d62844ef88fd640c5
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-11-01 21:55:33 -0500

    implementacion de algunas carpetas

---
+++ src/app/feature/historial/pages/historial/historial.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial.component.html',
+  styleUrl: './historial.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HistorialComponent {
+
+}
--- src/app/feature/home/pages/home/home.component.ts
+++ src/app/feature/home/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { MapsComponent } from "../../components/maps/maps.component";
+import { ModalRegisterComponent } from "../../../register/components/modal-register/modal-register.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MapsComponent, GoogleMapsModule],
+  imports: [MapsComponent, GoogleMapsModule, ModalRegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
