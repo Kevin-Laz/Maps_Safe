df6cfc9d41669c42d838cc7274cd036d38a0f0f9
Kevin Jhorman Liñan Ariza <162226502+Kevin-Laz@users.noreply.github.com>
2024-11-02 19:16:58 -0500

    Entornos (#7)
    * entornos vacios
    
    * environment dinamico
    
    * generacion del enviroment en producción
    
    * solucion error en la creacion del entorno
    
    * debug env.js
    
    * eliminacion mensajes

---
+++ env.js
@@ -0,0 +1,19 @@
+const { writeFileSync } = require('fs');
+const { MAPS_API_KEY } = process.env;
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `
+  export const environment = {
+    production: true,
+    googleMapsApiKey: '${MAPS_API_KEY}'
+  };
+`;
+
+writeFileSync(targetPath, envConfigFile, (err) => {
+  if (err) {
+    console.error('Error al generar environment.prod.ts', err);
+  } else {
+    console.log('environment.prod.ts generado correctamente');
+  }
+});
