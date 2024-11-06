2e5838da338cf25befc39bfc5a855cd10a5101e4
Laz-U <kevin-laz@outlook.com>
2024-11-05 20:12:29 -0500

    Creacion del contenedor busqueda y funcionalidad basica

--- src/app/feature/home/pages/home/home.component.ts
+++ src/app/feature/home/pages/home/home.component.ts
@@ -2,15 +2,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { MapsComponent } from "../../components/maps/maps.component";
 import { ModalRegisterComponent } from "../../../register/components/modal-register/modal-register.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MapsComponent, GoogleMapsModule, ModalRegisterComponent],
+  imports: [MapsComponent, GoogleMapsModule, ModalRegisterComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class HomeComponent {
-
+  origin:string = '';
+  destination:string = '';
+  searchRoute(){
+    console.log(this.destination);
+  }
 }
