078d5262bb7e706ebc91f42611761c979c09f3cf
GuilleDB <diazbeltrang@gmail.com>
2024-11-12 13:42:47 -0600

    Creación de vistas de historial y graficos

---
+++ src/app/feature/graficos/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { GraficosComponent } from './pages/graficos/graficos.component';
+
+export const ROUTES: Routes = [
+  { path: 'graficos', component: GraficosComponent },
+];
