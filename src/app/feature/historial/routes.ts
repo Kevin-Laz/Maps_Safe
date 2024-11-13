8ad04e7aca9e8efc9d9429fc96be35987ee9ff3f
GuilleDB <83146081+GuilleDB@users.noreply.github.com>
2024-11-13 12:30:18 -0600

    Creación de vistas de historial y graficos (#9)
    Co-authored-by: Kevin Jhorman Liñan Ariza <162226502+Kevin-Laz@users.noreply.github.com>

---
+++ src/app/feature/historial/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HistorialComponent } from './pages/historial/historial.component';
+
+export const ROUTES: Routes = [
+  { path: 'historial', component: HistorialComponent },
+];
