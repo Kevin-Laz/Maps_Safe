751f4f6241f4e134f604a0abc82b542711ea1b87
GuilleDB <83146081+GuilleDB@users.noreply.github.com>
2024-11-13 12:30:18 -0600

    Creación de vistas de historial y graficos (#9)
    Co-authored-by: Kevin Jhorman Liñan Ariza <162226502+Kevin-Laz@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ROUTES as HOME_ROUTES } from './feature/home/routes';
-
+import { ROUTES as GRAFICOS_ROUTES } from './feature/graficos/routes';
+import { ROUTES as HISTORIAL_ROUTES } from './feature/historial/routes';
 export const routes: Routes = [
-  ...HOME_ROUTES
+  ...HOME_ROUTES,
+  ...GRAFICOS_ROUTES,
+  ...HISTORIAL_ROUTES,
+
 ];
---
+++ src/app/feature/graficos/pages/graficos/graficos.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graficos',
+  standalone: true,
+  imports: [],
+  templateUrl: './graficos.component.html',
+  styleUrl: './graficos.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GraficosComponent {
+
+}
