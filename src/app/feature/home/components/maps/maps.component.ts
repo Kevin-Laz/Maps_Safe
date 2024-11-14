f46d4f746517ca94b13ce9b3d0caa86d2ebc54a4
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-11-13 21:17:44 -0500

    Creacion del servicio geocoding y busqueda funcionalidad completa de busqueda de ruta

--- src/app/feature/home/components/maps/maps.component.ts
+++ src/app/feature/home/components/maps/maps.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { GoogleMapsService } from '../../../../shared/services/google-maps/google-maps.service';
 
 @Component({
@@ -9,12 +9,25 @@ import { GoogleMapsService } from '../../../../shared/services/google-maps/googl
   styleUrl: './maps.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MapsComponent implements AfterViewInit, OnDestroy {
+export class MapsComponent implements AfterViewInit, OnChanges {
   @Input() center: google.maps.LatLngLiteral = { lat: -12.0464, lng: -77.0428 };
+  test: google.maps.LatLngLiteral = {lat: -12.0466, lng: -77.0499}
+  @Input() originCoordinates: google.maps.LatLngLiteral | null = null;
+  @Input() destinationCoordinates: google.maps.LatLngLiteral | null = null;
   @ViewChild('map', { static: false }) mapContainer!: ElementRef;
   private map!: google.maps.Map;
+  private originMarker!: any;
+  private destinationMarker!: any;
+  private directionsService!: google.maps.DirectionsService;
+  private directionsRenderer!: google.maps.DirectionsRenderer;
 
   constructor(private googleMapsService: GoogleMapsService){}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['originCoordinates'] || changes['destinationCoordinates']) {
+      this.updateMarkers();
+      this.updateRoute();
+    }
+  }
 
   async ngAfterViewInit(): Promise<void> {
     try{
@@ -44,19 +57,73 @@ export class MapsComponent implements AfterViewInit, OnDestroy {
       streetViewControl: false,
       fullscreenControl: true
     });
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(this.map);
+  }
+  private updateMarkers(): void {
+    if (!this.map) return;
 
-    const customMarkerImg = document.createElement('img');
-    customMarkerImg.src = 'marcador.png';
-    customMarkerImg.style.width = '50px';
-    customMarkerImg.style.height = '50px';
-    new AdvancedMarkerElement({
-      position: this.center,
-      map: this.map,
-      title: 'Ubicación',
-      content: customMarkerImg,
-    });
+    // Eliminar marcadores previos si existen
+    if (this.originMarker) this.originMarker.map = null;
+    if (this.destinationMarker) this.destinationMarker.map = null;
+
+    // Crear nuevo marcador para el origen si hay coordenadas
+    if (this.originCoordinates) {
+      /*const customMarkerImg = document.createElement('img');
+      customMarkerImg.src = 'marcador.png';
+      customMarkerImg.style.width = '50px';
+      customMarkerImg.style.height = '50px';*/
+
+      this.originMarker = new google.maps.marker.AdvancedMarkerElement({
+        position: this.originCoordinates,
+        map: this.map,
+        title: 'Origen',
+        //content: customMarkerImg,
+      });
+      this.map.setCenter(this.originCoordinates); // Centrar el mapa en el origen
+    }
+
+    // Crear nuevo marcador para el destino si hay coordenadas
+    if (this.destinationCoordinates) {
+      /*const customMarkerImg = document.createElement('img');
+      customMarkerImg.src = 'marcador.png';
+      customMarkerImg.style.width = '50px';
+      customMarkerImg.style.height = '50px';*/
+
+      this.destinationMarker = new google.maps.marker.AdvancedMarkerElement({
+        position: this.destinationCoordinates,
+        map: this.map,
+        title: 'Destino',
+        //content: customMarkerImg,
+      });
+    }
   }
 
+  private updateRoute(): void {
+    if (!this.originCoordinates || !this.destinationCoordinates) return;
+
+    const origin = new google.maps.LatLng(this.originCoordinates.lat, this.originCoordinates.lng);
+    const destination = new google.maps.LatLng(this.destinationCoordinates.lat, this.destinationCoordinates.lng);
+
+    if (this.originMarker) this.originMarker.map = null;
+    if (this.destinationMarker) this.destinationMarker.map = null;
+
+    this.directionsService.route(
+      {
+        origin,
+        destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (response, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.directionsRenderer.setDirections(response);
+        } else {
+          console.error('Directions request failed due to ' + status);
+        }
+      }
+    );
+  }
   ngOnDestroy(): void {
     // Limpia el mapa y elimina eventos para evitar problemas en cambios de ruta
     if (this.map) {
@@ -65,4 +132,5 @@ export class MapsComponent implements AfterViewInit, OnDestroy {
     }
   }
 }
+
 
