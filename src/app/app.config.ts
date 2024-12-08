c58e557780a62450197b469cfbead93ab104eec5
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-12-07 23:38:00 -0500

    *Generar que el componente home conserve el estado al cambioo de rutas *Creacion de ruta not-found

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CustomeRouteReuseStrategy } from './data/class/CustomeRouteReuseStrategy';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+              provideRouter(routes), provideAnimationsAsync(),
+              provideHttpClient(),
+              { provide: RouteReuseStrategy, useClass: CustomeRouteReuseStrategy }]
 };
