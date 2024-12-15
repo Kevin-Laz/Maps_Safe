acfc0a549a6466de5ff560066b26a2d2f708efb0
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2024-12-14 23:14:57 -0500

    grafico de linea funcional

--- src/app/feature/graficos/pages/graficos/graficos.component.ts
+++ src/app/feature/graficos/pages/graficos/graficos.component.ts
@@ -19,31 +19,73 @@ declare const ApexCharts: any;
 })
 
 export default class GraficosComponent implements OnInit {
-  constructor( private crimeSummary: CrimeSummaryService){}
+  // Control de visibilidad de dropdowns
+  isDropdownVisibleLine = false;
+  isDropdownVisiblePie = false;
+
+  monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  constructor(private crimeSummary: CrimeSummaryService) {}
+
   ngOnInit(): void {
     this.loadPieChart();
     this.loadAreaChart();
   }
 
-  isDropdownVisibleLine = false;
-  isDropdownVisiblePie = false;
-
-  toggleDropdownLine(): void {
-    this.isDropdownVisibleLine = !this.isDropdownVisibleLine;
+  // Métodos para alternar dropdowns
+  toggleDropdown(type: 'line' | 'pie'): void {
+    if (type === 'line') {
+      this.isDropdownVisibleLine = !this.isDropdownVisibleLine;
+    } else {
+      this.isDropdownVisiblePie = !this.isDropdownVisiblePie;
+    }
   }
-  toggleDropdownPie(): void {
-    this.isDropdownVisiblePie = !this.isDropdownVisiblePie;
-  }
 
+  // Cargar el gráfico de área
   private loadAreaChart(): void {
-    const options = {
+    this.crimeSummary.getCrimeSummary(2020, 'line').subscribe({
+      next: (data) => {
+        const chartOptions = this.getLineChartOptions(
+          data.map((item) => this.monthNames[item.mes-1]),
+          data.map((item) => item.cantidad)
+        );
+        const chart = new ApexCharts(document.getElementById('area-chart'), chartOptions);
+        chart.render();
+      },
+      error: (err) => {
+        console.error('Error al cargar el gráfico de área:', err);
+      }
+    });
+  }
+
+  // Cargar el gráfico de pastel
+  private loadPieChart(): void {
+    this.crimeSummary.getCrimeSummary(2020, 'pie').subscribe({
+      next: (data) => {
+        const groupedData = this.groupPieChartData(data, 0.1); // Agrupar datos con un umbral del 10%
+        const chartOptions = this.getPieChartOptions(groupedData.labels, groupedData.series);
+        const chart = new ApexCharts(document.getElementById('pie-chart'), chartOptions);
+        chart.render();
+      },
+      error: (err) => {
+        console.error('Error al cargar el gráfico de pastel:', err);
+      }
+    });
+  }
+
+  // Opciones para el gráfico de línea
+  private getLineChartOptions(categories: string[], seriesData: number[]): any {
+    return {
       chart: {
         height: '100%',
         maxWidth: '100%',
         type: 'area',
         fontFamily: 'Inter, sans-serif',
         dropShadow: { enabled: false },
-        toolbar: { show: false },
+        toolbar: { show: false }
       },
       tooltip: { enabled: true, x: { show: false } },
       fill: {
@@ -52,149 +94,74 @@ export default class GraficosComponent implements OnInit {
           opacityFrom: 0.55,
           opacityTo: 0,
           shade: '#1C64F2',
-          gradientToColors: ['#1C64F2'],
-        },
+          gradientToColors: ['#1C64F2']
+        }
       },
       dataLabels: { enabled: false },
       stroke: { width: 6 },
       grid: {
         show: false,
         strokeDashArray: 4,
-        padding: { left: 2, right: 2, top: 0 },
+        padding: { left: 2, right: 2, top: 0 }
       },
       series: [
         {
-          name: 'New users',
-          data: [6500, 6418, 6456, 6526, 6356, 6456],
-          color: '#1A56DB',
-        },
+          name: 'Crimes count',
+          data: seriesData,
+          color: '#1A56DB'
+        }
       ],
       xaxis: {
-        categories: [
-          '01 February',
-          '02 February',
-          '03 February',
-          '04 February',
-          '05 February',
-          '06 February',
-          '07 February',
-        ],
+        categories: categories,
         labels: { show: false },
         axisBorder: { show: false },
-        axisTicks: { show: false },
+        axisTicks: { show: false }
       },
-      yaxis: { show: false },
+      yaxis: { show: false }
     };
-
-    const chart = new ApexCharts(document.getElementById('area-chart'), options);
-    chart.render();
   }
 
-
-  private loadPieChart(): void{
-    this.crimeSummary.getCrimeSummary(2020, 'pie').subscribe({
-      next: (data)=>{
-        // Calcular el total de crímenes
-      const total = data.reduce((sum, item) => sum + item.cantidad, 0);
-
-      // Agrupar valores menores al 5% en "Others"
-      const threshold = total * 0.10;
-      const groupedData = data.reduce(
-        (acc, item) => {
-          if (item.cantidad >= threshold) {
-            acc.labels.push(item.tipo_crimen);
-            acc.series.push(item.cantidad);
-          } else {
-            acc.othersCount += (item.cantidad);
-          }
-          return acc;
-        },
-        { labels: [] as string[], series: [] as number[], othersCount: 0 }
-      );
-
-      // Agregar "Others" si es necesario
-      if (groupedData.othersCount > 0) {
-        groupedData.labels.push('OTHERS');
-        groupedData.series.push(groupedData.othersCount);
-      }
-        const chartOptions = this.getChartOptions(groupedData.labels, (groupedData.series));
-        const chart = new ApexCharts(document.getElementById("pie-chart"), chartOptions);
-        chart.render();
-      },
-      error: (err)=>{
-        console.log("Error al cargar los datos del gráfico", err);
-      }
-    })
-  }
-
-  private getChartOptions(labels: string[], series: number[]) {
+  // Opciones para el gráfico de pastel
+  private getPieChartOptions(labels: string[], series: number[]): any {
     return {
       series: series,
       colors: [
-        "#1C64F2", // Azul oscuro
-        "#16BDCA", // Turquesa
-        "#9061F9", // Lila
-        "#F59E0B", // Amarillo oscuro
-        "#10B981", // Verde esmeralda
-        "#EF4444", // Rojo brillante
-        "#3B82F6", // Azul claro
-        "#6366F1", // Azul violeta
-        "#F472B6", // Rosa vibrante
-        "#8B5CF6"  // Púrpura
-      ]
-      ,
-      chart: {
-        height: 420,
-        width: "100%",
-        type: "pie",
-      },
-      stroke: {
-        colors: ["white"],
-        lineCap: "round",
-      },
-      plotOptions: {
-        pie: {
-          labels: {
-            show: true,
-          },
-          size: "100%",
-          dataLabels: {
-            offset: -25
-          }
-        },
-      },
+        "#1C64F2", "#16BDCA", "#9061F9", "#F59E0B", "#10B981",
+        "#EF4444", "#3B82F6", "#6366F1", "#F472B6", "#8B5CF6"
+      ],
+      chart: { height: 420, width: '100%', type: 'pie' },
+      stroke: { colors: ['white'], lineCap: 'round' },
+      plotOptions: { pie: { size: '100%' } },
       labels: labels,
-      dataLabels: {
-        enabled: true,
-        style: {
-          fontFamily: "Inter, sans-serif",
-        },
-      },
-      legend: {
-        position: "bottom",
-        fontFamily: "Inter, sans-serif",
-      },
-      yaxis: {
-        labels: {
-          formatter: function (value: number) {
-            return value;
-          },
-        },
-      },
-      xaxis: {
-        labels: {
-          formatter: function (value: number) {
-            return value + "%";
-          },
-        },
-        axisTicks: {
-          show: false,
-        },
-        axisBorder: {
-          show: false,
-        },
-      },
+      dataLabels: { enabled: true, style: { fontFamily: 'Inter, sans-serif' } },
+      legend: { position: 'bottom', fontFamily: 'Inter, sans-serif' }
     };
+  }
+
+  // Método para agrupar datos menores a un umbral
+  private groupPieChartData(data: any[], thresholdPercentage: number): { labels: string[], series: number[] } {
+    const total = data.reduce((sum, item) => sum + item.cantidad, 0);
+    const threshold = total * thresholdPercentage;
+
+    const groupedData = data.reduce(
+      (acc, item) => {
+        if (item.cantidad >= threshold) {
+          acc.labels.push(item.tipo_crimen);
+          acc.series.push(item.cantidad);
+        } else {
+          acc.othersCount += item.cantidad;
+        }
+        return acc;
+      },
+      { labels: [] as string[], series: [] as number[], othersCount: 0 }
+    );
+
+    if (groupedData.othersCount > 0) {
+      groupedData.labels.push('OTHERS');
+      groupedData.series.push(groupedData.othersCount);
+    }
+
+    return { labels: groupedData.labels, series: groupedData.series };
   }
 
 }
